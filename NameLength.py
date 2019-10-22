b4afd5fc59fd6f750f85412d5c25b64c95a4b0e6
liu215229932 <47161303+liu215229932@users.noreply.github.com>
2019-10-22 14:51:02 -0400

    Add files via upload

---
+++ NameLength.py
@@ -0,0 +1,28 @@
+print ("Hi")
+
+first_name = input ("Enter first name please")
+last_name = input ("Enter last nameplease")
+print("Hi, " + first_name + " " + last_name)
+
+#more string variables
+first_name2="ABC"
+last_name2="XYZ"
+
+#Intigers
+a=10
+print(a+1)
+print(a+10)
+print(a**2)
+
+
+give_me_a_number = input ("Enter a number please ")
+print(int(give_me_a_number)*20)#typecasting function
+
+first_name = input ("Enter first name please? ")
+length_of_name=len(first_name)
+print(length_of_name)
+
+first_name = input ("Enter first name please ")
+last_name = input ("Enter last nameplease ")
+length_of_name=len(first_name+last_name)
+print(((length_of_name*2+9-3)/2)-length_of_name)
